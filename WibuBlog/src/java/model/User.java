e72b8c1eb80fc69d7db3c37b450871b3c68ba6ba
Lolrelic12 <44398441+Lolrelic12@users.noreply.github.com>
2024-05-22 22:11:59 +0700

    added models

---
+++ WibuBlog/src/java/model/User.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author admin
+ */
+public class User {
+    private int userId;
+    private String username;
+    private String passwordHash;
+    private int roleId;
+    private int point;
+    private String status;
+    private String email;
+    private String fullName;
+    private int rankId;
+
+    public User() {
+    }
+
+    public User(int userId, String username, String passwordHash, int roleId, int point, String status, String email, String fullName, int rankId) {
+        this.userId = userId;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.roleId = roleId;
+        this.point = point;
+        this.status = status;
+        this.email = email;
+        this.fullName = fullName;
+        this.rankId = rankId;
+    }
+    
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public boolean verifyPassword(String hash) {
+        return passwordHash.equals(hash);
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+    
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public void addPoint(int point) {
+        this.point += point;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getRankId() {
+        return rankId;
+    }
+
+    public void setRankId(int rankId) {
+        this.rankId = rankId;
+    }
+    
+    
+    
+}

2e956e08404cde67d2f25dcafca5d08176745e11
Lolrelic12 <44398441+Lolrelic12@users.noreply.github.com>
2024-05-23 21:23:10 +0700

    email verification hotfix

--- WibuBlog/src/java/controller/ForgotPassword.java
+++ WibuBlog/src/java/controller/ForgotPassword.java
@@ -20,9 +20,6 @@ import com.ContentDelivery;
 @WebServlet(name = "ForgotPassword", urlPatterns = {"/ForgotPassword"})
 public class ForgotPassword extends HttpServlet {
 
-    private String verificationCode = "";
-    private String email = "";
-
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -43,12 +40,15 @@ public class ForgotPassword extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        email = request.getParameter("email");
+        
+        String email = request.getParameter("email");
         UserDAO ud = new UserDAO();
 
         if (ud.emailExists(email)) {
-            verificationCode = KeyGenerator.generateVerificationCode();
-            ContentDelivery.sendVerificationCode("test", email, verificationCode);
+            String verificationCode = KeyGenerator.generateVerificationCode();
+            ContentDelivery.sendVerificationCode("Verification code", email, verificationCode);
+            request.setAttribute("email", email);
+            request.setAttribute("template", verificationCode);
             request.getRequestDispatcher("authenticate.jsp").forward(request, response);
         } else {
             String errorMessage = "This email does not exist!";
@@ -61,16 +61,19 @@ public class ForgotPassword extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String submitted = request.getParameter("verificationCode");
-        email = request.getParameter("email");
-
-        if (submitted.equals(verificationCode)) {
+        
+        String email = request.getParameter("email");
+        String verificationCode = request.getParameter("template");
+        String input = request.getParameter("response");
+        
+        if (input.equals(verificationCode)) {
             request.setAttribute(email, "email");
             request.getRequestDispatcher("resetpassword.jsp").forward(request, response);
         } else {
             String errorMessage = "Incorrect verification code.";
             request.setAttribute(errorMessage, "errorMessage");
             request.setAttribute(email, "email");
+            request.setAttribute("template", verificationCode);
             request.getRequestDispatcher("authenticate.jsp").forward(request, response);
         }
 

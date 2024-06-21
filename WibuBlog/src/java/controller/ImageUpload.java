cb90a2fcfcedbb6df41a314194b8040f2c60cf56
QuocAnh1502 <anhcnqhe187233@fpt.edu.vn>
2024-06-21 14:49:10 +0700

    a

--- WibuBlog/src/java/controller/ImageUpload.java
+++ WibuBlog/src/java/controller/ImageUpload.java
@@ -4,6 +4,8 @@
  */
 package controller;
 
+import dal.MediaDAO;
+import dal.PostDAO;
 import utility.ImageHandler;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,6 +15,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.Part;
 import java.awt.image.BufferedImage;
 import java.io.InputStream;
@@ -23,6 +26,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import javax.imageio.ImageIO;
+import model.Post;
+import model.User;
 
 /**
  *
@@ -60,7 +65,9 @@ public class ImageUpload extends HttpServlet {
             throws ServletException, IOException {
         // Lấy đường dẫn thực tế đến thư mục gốc của ứng dụng web
         String realPath = request.getServletContext().getRealPath("/");
-
+        MediaDAO mediaDAO = new MediaDAO();
+        HttpSession session = request.getSession();
+        User user = (User)session.getAttribute("user");
         // Xây dựng đường dẫn tuyệt đối đến thư mục images
         Path baseImagePath = Paths.get(realPath, "images");
 
@@ -73,38 +80,43 @@ public class ImageUpload extends HttpServlet {
 
         // Lưu danh sách các file ảnh và tên file
         List<String> fileNames = new ArrayList<>();
-
+         
         // Lấy các file ảnh từ request và lưu vào thư mục "game"
         for (Part part : request.getParts()) {
             String submittedFileName = part.getSubmittedFileName();
             if (submittedFileName != null && ImageHandler.isImageFile(submittedFileName)) {
                 fileNames.add(submittedFileName);
-
+                String encodedMediaName = mediaDAO.encodeMediaName(user.getUserId()) + "." + ImageHandler.getExtension(submittedFileName);
+                PostDAO postDAO = new PostDAO();
+                Post post = (Post)session.getAttribute("newPost");
+                post.setImage("http://localhost:9999/WibuBlog/images/game/" + encodedMediaName);
+                int postID = postDAO.getPostIDJustInserted(user.getUserId());
+                postDAO.setPostImage("http://localhost:9999/WibuBlog/images/game/" + encodedMediaName, postID);
+                session.removeAttribute("newPost");
                 // Đọc và lưu ảnh vào thư mục "game"
                 try (InputStream input = part.getInputStream()) {
                     BufferedImage image = ImageIO.read(input);
                     ImageHandler.saveImage(image, gameDirectory.toString(),
-                            submittedFileName, ImageHandler.getExtension(submittedFileName));
+                            encodedMediaName, ImageHandler.getExtension(encodedMediaName));
                 } catch (IOException e) {
                     response.getWriter().println("Error reading or saving image: " + e.getMessage());
                     return;
                 }
             }
         }
-
-        // Tạm dừng 2 giây để đảm bảo hình ảnh được lưu trước khi forward
+        
+        response.sendRedirect("Home.jsp");
+        
         try {
-            Thread.sleep(2000);
+            Thread.sleep(2);
         } catch (InterruptedException e) {
-            // Handle interrupted exception if needed
-        }
 
-        // Gửi phản hồi về client
+        }
+        
+        
         response.getWriter().println("Upload thành công các ảnh vào thư mục: " + gameDirectory.toString());
 
-        // Sau khi xử lý xong, chuyển hướng về trang Home.jsp hoặc trang khác
-//        request.setAttribute("image", fileNames.isEmpty() ? null : fileNames);
-//        request.getRequestDispatcher("Home.jsp").forward(request, response);
+  
     }
 
     /**
